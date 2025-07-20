0a421c02b0ee2adff2fa7d0666ce0b53060eee95
Andree <andreeunleashed@gmail.com>
2025-07-20 18:00:36 -0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,8 +19,7 @@ dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.7.3")
-    implementation("org.jetbrains.kotlin:kotlin-reflect:1.9.22")
-    implementation("org.slf4j:slf4j-simple:2.0.7")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:2.1.21")
 }
 
 tasks.test {
